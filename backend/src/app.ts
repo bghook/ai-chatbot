775de9dbfb659870d25b6d0eaa2f7301589b7170
bghook <146375034+bghook@users.noreply.github.com>
2024-06-06 20:43:11 -0500

    Update frontend URLs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,10 @@ const app = express();
 // Middleware - functions that have access to the request and response objects
 // app.use(cors({ origin: "http://localhost:5173", credentials: true })); // Adding the server which hosts our app to whitelist
 app.use(
-  cors({ origin: "vercel.app", credentials: true }) // frontend URL
+  cors({
+    origin: "https://ai-chatbot-front-end.vercel.app",
+    credentials: true,
+  }) // frontend URL
 ); // Adding the server which hosts our app to whitelist
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
